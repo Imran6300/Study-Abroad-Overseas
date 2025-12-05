bb2f04011ce0cd3fc2f073b2191748d344df7ebe
Imran6300 <syedimran8742@gmail.com>
2025-12-05 23:40:43 +0530

    tsx fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import "./globals.css";
 import NavBar from "../components/Header/nav-bar";
 import Footer from "../components/Footer/Footer";
+import type { ReactNode } from "react";
 
 export const metadata = {
   title: "Overseas",
   description: "Study Abroad Website",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="overflow-x-hidden">
