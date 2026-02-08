328986ce12f2b25c654dae3b89994b298e71986a
Imran6300 <syedimran8742@gmail.com>
2026-02-08 19:02:02 +0530

    removed images

--- components/Home/SuccessStories/SuccessData.jsx
+++ components/Home/SuccessStories/SuccessData.jsx
@@ -8,7 +8,7 @@ export const Data = [
     visa: "Approved",
     scholarship: "Merit-Based Scholarship",
     year: "2021",
-    image: "/student/student1.jpg",
+    // image: "/student/student1.jpg",
     review:
       "Khizar Overseas supported me at every step of my study abroad journey. From university shortlisting to visa guidance, the process was smooth and well-organized. Their support helped me confidently start my academic life in the UK.",
   },
@@ -21,7 +21,7 @@ export const Data = [
     visa: "Approved",
     scholarship: "University Grant",
     year: "2021",
-    image: "/student/student1.jpg",
+    // image: "/student/student1.jpg",
 
     review:
       "The counselors provided excellent guidance for course selection and documentation. The visa preparation and mock interviews made the process stress-free and successful.",
@@ -35,7 +35,7 @@ export const Data = [
     visa: "Approved in 1st Attempt",
     scholarship: "Merit-Based Scholarship",
     year: "2022",
-    image: "/student/student1.jpg",
+    // image: "/student/student1.jpg",
 
     review:
       "Applying to top universities like MIT felt overwhelming at first, but Khizar Overseas handled everything professionally. Their structured approach made a big difference.",
@@ -49,7 +49,7 @@ export const Data = [
     visa: "Approved",
     scholarship: "Need-Based Financial Aid",
     year: "2024",
-    image: "/student/student1.jpg",
+    // image: "/student/student1.jpg",
 
     review:
       "From SOP drafting to visa filing, the team ensured every detail was perfect. Their honest advice and constant support helped me secure admission at Harvard.",
