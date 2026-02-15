f442a437ad2fec0a20606f72122ed46165d5f32e
Imran6300 <syedimran8742@gmail.com>
2026-02-15 12:37:59 +0530

    added google GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 import ReduxProvider from "@/store/ReduxProvider";
 import AuthInitializer from "@/components/AuthInitializer";
 import AuthGate from "@/components/AuthGate";
+import Script from "next/script";
 
 export const metadata = {
   title: {
@@ -25,7 +26,22 @@ export default function RootLayout({
             <AuthGate>{children}</AuthGate>
           </AuthInitializer>
         </ReduxProvider>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1W7JC83PF0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1W7JC83PF0');
+          `}
+        </Script>
       </body>
     </html>
   );
 }
+
