4bf2907c41450447134c316081397270c19ed86d
Imran6300 <syedimran8742@gmail.com>
2026-02-25 02:51:47 +0530

    fixed the useSearchParams error

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GoogleAnalytics() {
     const pathname = usePathname();
-    const searchParams = useSearchParams();
 
     useEffect(() => {
-        const url = pathname + searchParams.toString();
-
-        window.gtag?.("config", "G-1W7JC83PF0", {
-            page_path: url,
-        });
-    }, [pathname, searchParams]);
+        if (typeof window !== "undefined" && window.gtag) {
+            window.gtag("config", "G-1W7JC83PF0", {
+                page_path: pathname,
+            });
+        }
+    }, [pathname]);
 
     return null;
 }
