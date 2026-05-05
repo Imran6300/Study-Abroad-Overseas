45b0318157e4060fb2aa5f37ea575d62a5264f2d
Imran6300 <syedimran8742@gmail.com>
2026-05-05 22:17:06 +0530

    updated the sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,24 +3,31 @@ export default async function sitemap() {
 
   let allUniversities = [];
 
-  for (let i = 1; i <= 10; i++) {
-    // start with 10 pages (later increase)
+  const firstRes = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/universities?page=1`,
+  );
+  const firstData = await firstRes.json();
+
+  const totalPages = firstData.totalPages;
+
+  allUniversities.push(...firstData.universities);
+
+  for (let i = 2; i <= totalPages; i++) {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/universities?page=${i}`,
     );
     const data = await res.json();
-
     allUniversities.push(...data.universities);
   }
 
   const universityUrls = allUniversities.map((uni) => ({
-    url: `${baseUrl}/universities/${uni.slug}`,
+    url: `${baseUrl}/programs/universities/${uni.slug}`,
     lastModified: new Date(),
   }));
 
   return [
     {
-      url: `${baseUrl}/universities`,
+      url: `${baseUrl}/programs/universities`,
       lastModified: new Date(),
     },
     ...universityUrls,
