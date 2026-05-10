b0f0c52db3931434be3100e4e8d004c45e78344a
Imran6300 <syedimran8742@gmail.com>
2026-05-10 21:51:18 +0530

    changed the main sitemap path

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,18 @@
+export async function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://www.khizaroverseas.in/sitemap-universities.xml</loc>
+  </sitemap>
+
+  <sitemap>
+    <loc>https://www.khizaroverseas.in/sitemap-countries.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
