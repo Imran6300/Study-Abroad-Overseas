29f716dee5439427d13a73b0fc66acc778d054de
Imran6300 <syedimran8742@gmail.com>
2026-05-11 14:49:22 +0530

    again changed the structure to find out real root couse

--- app/sitemap-universities/[page]/route.js
+++ app/sitemap-universities/[page]/route.js
@@ -1,7 +1,8 @@
-export async function GET(req, { params }) {
+export async function GET(req, context) {
+  const { page } = await context.params;
+
   return Response.json({
     success: true,
-    params,
-    page: params.page,
+    page,
   });
 }
